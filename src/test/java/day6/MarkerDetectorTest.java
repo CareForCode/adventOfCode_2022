b2dad429fb3cee68e655e10bda761616988fe288
patrickscheibert <patrick.scheibert@outlook.com>
2022-12-26 20:41:15 +0100

    day6: get first result

--- src/test/java/day6/MarkerDetectorTest.java
+++ src/test/java/day6/MarkerDetectorTest.java
@@ -1,7 +1,12 @@
 package day6;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,5 +18,25 @@ class MarkerDetectorTest {
     @CsvSource(value = {"mjqjpqmgbljsphdztnvjfqwrcgsmlb,7", "bvwbjplbgvbhsrlpgdmjqwftvncz,5", "nppdvjthqldpwncqszvftbrmjlhg, 6"})
     void detectMarkerPosition(String input, int expectedMarkerPosition) {
         assertThat(markerDetector.detectMarkerPosition(input)).isEqualTo(expectedMarkerPosition);
+    }
+
+    @Test
+    void result1() {
+        BufferedReader reader;
+        int result = 0;
+
+        try {
+            reader = new BufferedReader(new FileReader("src/test/resources/adventOfCode_2022_6_Input.txt"));
+            String line = reader.readLine();
+            while (line != null) {
+                result = markerDetector.detectMarkerPosition(line);
+                // read next line
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assertThat(result).isEqualTo(1909);
     }
 }
